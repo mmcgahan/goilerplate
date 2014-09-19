d46b22aa7fc5eabaf856d78209f276678d34f38a
Michael McGahan <mikem@meetup.com>
2014-09-19 17:54:22 -0400

    basic setup

---
+++ server.go
@@ -0,0 +1,52 @@
+package main
+
+import (
+	"fmt"
+	"github.com/gorilla/mux"
+	"html/template"
+	"net/http"
+)
+
+type Page struct {
+	Title string
+	Body  string
+}
+
+func IndexHandler(w http.ResponseWriter, r *http.Request) {
+	// gather URL vars from mux
+	v := mux.Vars(r)
+
+	title, ok := v["title"]
+	if !ok {
+		title = "This"
+	}
+
+	// build Page struct for template
+	p := &Page{Title: title, Body: "This & that"}
+
+	// load template
+	t, err := template.ParseFiles("templates/index.html")
+	if err != nil {
+		panic(err)
+	}
+
+	// build template
+	err = t.ExecuteTemplate(w, "index.html", p)
+	if err != nil {
+		panic(err)
+	}
+}
+
+func main() {
+	fmt.Printf("Setting up routes ... \n")
+	r := mux.NewRouter()
+	r.HandleFunc("/", IndexHandler)
+	r.HandleFunc("/{title}", IndexHandler)
+	http.Handle("/", r)
+
+	fmt.Printf("Starting http Server ...\n")
+	err := http.ListenAndServe("0.0.0.0:8000", nil)
+	if err != nil {
+		fmt.Errorf("ListenAndServe Error :" + err.Error())
+	}
+}
